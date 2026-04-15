59e35b540afd69fcef28401a445b9fcf0141a341
MadaminovJamshidbek <jmadaminov.unlimited@gmail.com>
2026-04-15 10:59:49 +0500

    Verification o'chirildi !

--- src/main/java/com/example/shajara/service/auth/AuthService.java
+++ src/main/java/com/example/shajara/service/auth/AuthService.java
@@ -1,6 +1,5 @@
 package com.example.shajara.service.auth;
 
-
 import com.example.shajara.dto.AppResponse;
 import com.example.shajara.dto.ProfileDTO;
 import com.example.shajara.dto.auth.AuthDTO;
@@ -53,232 +52,231 @@ public class AuthService {
     @Autowired
     private ProfileRoleRepository profileRoleRepository;
 
-
     @Autowired
     private SmsHistoryService smsHistoryService;
 
     @Autowired
     private EmailHistoryService emailHistoryService;
 
-
     private static Logger log = LoggerFactory.getLogger(AuthService.class);
 
-    public AppResponse<String> registration(RegistrationDTO dto){
-        //1.validation buni keyin tekshiramiz
-        //2.email bor yo'qligi tizimda
-        //inregist bo'lgan bo'lsa shartmanda
-        Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(dto.getUsername());
-        if(optional.isPresent()){
-            ProfileEntity profile= optional.get();
-            if(profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)){   //menga id kerakda shunga o'chirib yubormadim
+    public AppResponse<String> registration(RegistrationDTO dto) {
+        // 1.validation buni keyin tekshiramiz
+        // 2.email bor yo'qligi tizimda
+        // inregist bo'lgan bo'lsa shartmanda
+        Optional<ProfileEntity> optional = profileRepository.findByUsernameAndVisibleTrue(dto.getUsername());
+        if (optional.isPresent()) {
+            ProfileEntity profile = optional.get();
+            if (profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)) { // menga id kerakda shunga o'chirib
+                                                                             // yubormadim
                 profileRoleService.deleteRoll(profile.getId());
 
-                //send sms/email
-                ProfileEntity entity=optional.get();
+                // send sms/email
+                ProfileEntity entity = optional.get();
                 entity.setName(dto.getName());
                 entity.setPassword(bc.encode(dto.getPassword()));
                 entity.setUsername(dto.getUsername());
-                entity.setStatus(GeneralStatus.IN_REGISTRATION);
+                // entity.setStatus(GeneralStatus.IN_REGISTRATION);
+                entity.setStatus(GeneralStatus.ACTIVE);
                 entity.setVisible(true);
                 entity.setCreatedDate(LocalDateTime.now());
-                profileRepository.save(entity);//save
-                //insert role
+                profileRepository.save(entity);// save
+                // insert role
                 profileRoleService.create(entity.getId(), ProfileRole.ROLE_USER);
-                if(EmailUtil.isEmail(dto.getUsername())){
-                    emailSendingService.sendRegistrationEmail(dto.getUsername(),entity.getId());}
-                else if(SmsUtil.isPhone(dto.getUsername())) {
-                    //todo
-                    throw new AppBadException("Sms provider ishlamayapti.Email orqali registratsiya qiling !");
-                    //smsSendingService.registrationSms(dto.getUsername());
-                }else{
+                if (EmailUtil.isEmail(dto.getUsername())) {
+                    // emailSendingService.sendRegistrationEmail(dto.getUsername(),entity.getId());
+                } else if (SmsUtil.isPhone(dto.getUsername())) {
+                    // todo
+                    // throw new AppBadException("Sms provider ishlamayapti.Email orqali
+                    // registratsiya qiling !");
+                    // smsSendingService.registrationSms(dto.getUsername());
+                } else {
                     throw new AppBadException("wrong username !");
                 }
-                return new AppResponse<>("Activation link send to your email");
+                // return new AppResponse<>("Activation link send to your email");
+                return new AppResponse<>("Tabriklaymiz ,Account created ,Login qilib kiring !");
 
-            }
-            else {
-                log.warn("Profile already exist with username : {}",dto.getUsername());
-                //throw new AppBadException(messageSource.getMessage("email.phone.exist",null,new Locale(lang.name())));
+            } else {
+                log.warn("Profile already exist with username : {}", dto.getUsername());
+                // throw new
+                // AppBadException(messageSource.getMessage("email.phone.exist",null,new
+                // Locale(lang.name())));
                 throw new AppBadException("Email or Phone  already exist !");
             }
         }
 
-        ProfileEntity entity=new ProfileEntity();
+        ProfileEntity entity = new ProfileEntity();
         entity.setName(dto.getName());
         entity.setPassword(bc.encode(dto.getPassword()));
         entity.setUsername(dto.getUsername());
         entity.setStatus(GeneralStatus.IN_REGISTRATION);
         entity.setVisible(true);
         entity.setCreatedDate(LocalDateTime.now());
-        profileRepository.save(entity);//save
-        //insert role
+        profileRepository.save(entity);// save
+        // insert role
         profileRoleService.create(entity.getId(), ProfileRole.ROLE_USER);
-        if(EmailUtil.isEmail(dto.getUsername())){
-        emailSendingService.sendRegistrationEmail(dto.getUsername(),entity.getId());}
-        else if(SmsUtil.isPhone(dto.getUsername())) {
-            //todo
+        if (EmailUtil.isEmail(dto.getUsername())) {
+            emailSendingService.sendRegistrationEmail(dto.getUsername(), entity.getId());
+        } else if (SmsUtil.isPhone(dto.getUsername())) {
+            // todo
             throw new AppBadException("Sms provider ishlamayapti.Email orqali registratsiya qiling !");
-            //smsSendingService.registrationSms(dto.getUsername());
-        }else{
+            // smsSendingService.registrationSms(dto.getUsername());
+        } else {
             throw new AppBadException("wrong username !");
         }
         return new AppResponse<>("Activation link send to your email");
     }
 
-    //todo cheksiz bosib o'tiravarmasligi uchun nima qilay buni
-    public String registrationEmailVerification(String token){
-    try{
-        Integer profileId= JwtUtil.decodeRegVerToken(token);//idni email serviceda shifrlagandikku o'sha uchun
-        ProfileEntity profile=profileService.getById(profileId);
-        if(profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)){//block yana active qilmidi linkdan
+    // todo cheksiz bosib o'tiravarmasligi uchun nima qilay buni
+    public String registrationEmailVerification(String token) {
+        try {
+            Integer profileId = JwtUtil.decodeRegVerToken(token);// idni email serviceda shifrlagandikku o'sha uchun
+            ProfileEntity profile = profileService.getById(profileId);
+            if (profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)) {// block yana active qilmidi linkdan
 
-            profileRepository.changeStatus(profileId,GeneralStatus.ACTIVE);
-            return "Verification finished !";
+                profileRepository.changeStatus(profileId, GeneralStatus.ACTIVE);
+                return "Verification finished !";
+            }
+        } catch (JwtException e) {
+            e.printStackTrace();
         }
-    }catch(JwtException e){
-        e.printStackTrace();
-    }
         log.warn("Registration Email Verification failed !");
         throw new AppBadException("Verification failed !");
 
     }
 
-    public ProfileDTO login(AuthDTO dto){
-        Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(dto.getUsername());
-        if(optional.isEmpty()){
-            log.warn(" username wrong : {}",dto.getUsername());
+    public ProfileDTO login(AuthDTO dto) {
+        Optional<ProfileEntity> optional = profileRepository.findByUsernameAndVisibleTrue(dto.getUsername());
+        if (optional.isEmpty()) {
+            log.warn(" username wrong : {}", dto.getUsername());
             throw new AppBadException("Username is wrong");
         }
-        if(! optional.get().getStatus().equals(GeneralStatus.ACTIVE)){
-            log.warn("Wrong status  ");//xuddi shunaqa qolgan joylarda ham yozib chiqiladi
+        if (!optional.get().getStatus().equals(GeneralStatus.ACTIVE)) {
+            log.warn("Wrong status  ");// xuddi shunaqa qolgan joylarda ham yozib chiqiladi
             throw new AppBadException("Status is wrong !");
         }
 
-        if(! bc.matches(dto.getPassword(), optional.get().getPassword())){//matches bcrypda tenglika tekshiriberadi.
+        if (!bc.matches(dto.getPassword(), optional.get().getPassword())) {// matches bcrypda tenglika tekshiriberadi.
             throw new AppBadException("Password is wrong");
         }
 
-
-        ProfileEntity profile=optional.get();
-        //response
+        ProfileEntity profile = optional.get();
+        // response
         return getLogInResponse(profile);
     }
 
-    public ProfileDTO registrationSmsVerification(SmsVerificationDTO dto){
-        //todo
+    public ProfileDTO registrationSmsVerification(SmsVerificationDTO dto) {
+        // todo
         throw new AppBadException("Sms provider ishlamayapti.Email orqali registratsiya qiling !");
-        //phone and code:12345
-//
-//        Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(dto.getPhone());
-//        if(optional.isEmpty()){
-//            throw new AppBadException("phone not exist !");
-//        }
-//        ProfileEntity profile= optional.get();
-//        if(!profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)){
-//            throw new AppBadException("status wrong !");
-//        }
-//        //code check
-//        if(!smsHistoryService.check(dto.getPhone(), dto.getCode())){
-//            throw new AppBadException("code wrong !");
-//        }
-//
-//        //ACTIVE
-//        profileRepository.changeStatus(profile.getId(),GeneralStatus.ACTIVE);
-//        //smsni tasdiqlaagandan keyin qayta login qilib o'tirmasin to'g'ri kirsin accountiga
-//
-//
-//        return getLogInResponse(profile);
+        // phone and code:12345
+        //
+        // Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(dto.getPhone());
+        // if(optional.isEmpty()){
+        // throw new AppBadException("phone not exist !");
+        // }
+        // ProfileEntity profile= optional.get();
+        // if(!profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)){
+        // throw new AppBadException("status wrong !");
+        // }
+        // //code check
+        // if(!smsHistoryService.check(dto.getPhone(), dto.getCode())){
+        // throw new AppBadException("code wrong !");
+        // }
+        //
+        // //ACTIVE
+        // profileRepository.changeStatus(profile.getId(),GeneralStatus.ACTIVE);
+        // //smsni tasdiqlaagandan keyin qayta login qilib o'tirmasin to'g'ri kirsin
+        // accountiga
+        //
+        //
+        // return getLogInResponse(profile);
 
-
-        //return resourceBundleService.message("verif.finish",lang);
+        // return resourceBundleService.message("verif.finish",lang);
     }
 
-
-
-    public String registrationSmsVerificationResend(SmsResendDTO dto){
-        //todo
+    public String registrationSmsVerificationResend(SmsResendDTO dto) {
+        // todo
         throw new AppBadException("Sms provider ishlamayapti.Email orqali registratsiya qiling !");
-        //phone and code:12345
-//
-//        Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(dto.getPhone());
-//        if(optional.isEmpty()){
-//            throw new AppBadException("phone not exist !");
-//        }
-//        ProfileEntity profile= optional.get();
-//        if(!profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)){
-//            throw new AppBadException("status wrong !");
-//        }
-//        //smsSendingService.registrationSms(dto.getPhone(),lang);//sms jo'nat degani
-//
-//        return "yangi sms muaffiqiyatli qayta jo'natildi !";
+        // phone and code:12345
+        //
+        // Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(dto.getPhone());
+        // if(optional.isEmpty()){
+        // throw new AppBadException("phone not exist !");
+        // }
+        // ProfileEntity profile= optional.get();
+        // if(!profile.getStatus().equals(GeneralStatus.IN_REGISTRATION)){
+        // throw new AppBadException("status wrong !");
+        // }
+        // //smsSendingService.registrationSms(dto.getPhone(),lang);//sms jo'nat degani
+        //
+        // return "yangi sms muaffiqiyatli qayta jo'natildi !";
     }
 
     public AppResponse<String> resetPassword(ResetPasswordDTO dto) {
-        String username=dto.getUsername();
-        //check
-        Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(username);
-        if(optional.isEmpty()){
+        String username = dto.getUsername();
+        // check
+        Optional<ProfileEntity> optional = profileRepository.findByUsernameAndVisibleTrue(username);
+        if (optional.isEmpty()) {
             throw new AppBadException("phone not exist !");
         }
-        ProfileEntity profile= optional.get();
-        if(!profile.getStatus().equals(GeneralStatus.ACTIVE)){
+        ProfileEntity profile = optional.get();
+        if (!profile.getStatus().equals(GeneralStatus.ACTIVE)) {
             throw new AppBadException("status wrong !");
         }
 
-        if(EmailUtil.isEmail(dto.getUsername())){
-            emailSendingService.sendResetPasswordEmail(dto.getUsername());}
-        else if(SmsUtil.isPhone(dto.getUsername())) {
-            //todo
+        if (EmailUtil.isEmail(dto.getUsername())) {
+            emailSendingService.sendResetPasswordEmail(dto.getUsername());
+        } else if (SmsUtil.isPhone(dto.getUsername())) {
+            // todo
             throw new AppBadException("Sms provider ishlamayapti.Email orqali  qiling !");
-            //smsSendingService.sendResetPasswordSms(dto.getUsername());
-        }else{
+            // smsSendingService.sendResetPasswordSms(dto.getUsername());
+        } else {
             throw new AppBadException("wrong username !");
         }
 
-        return new AppResponse<>("Tasdiqlash kodi "+username+"ga jo'natildi");
+        return new AppResponse<>("Tasdiqlash kodi " + username + "ga jo'natildi");
     }
 
-    public AppResponse<String> resetPasswordConfirm(ResetPasswordConfirmDTO dto) {//parolni o'zgartirayotganda tasdiqlash kodni
+    public AppResponse<String> resetPasswordConfirm(ResetPasswordConfirmDTO dto) {// parolni o'zgartirayotganda
+                                                                                  // tasdiqlash kodni
 
-        Optional<ProfileEntity>optional=profileRepository.findByUsernameAndVisibleTrue(dto.getUsername());
-        if(optional.isEmpty()){
+        Optional<ProfileEntity> optional = profileRepository.findByUsernameAndVisibleTrue(dto.getUsername());
+        if (optional.isEmpty()) {
             throw new AppBadException("phone not exist !");
         }
 
-        ProfileEntity profile= optional.get();
-        if(!profile.getStatus().equals(GeneralStatus.ACTIVE)){
+        ProfileEntity profile = optional.get();
+        if (!profile.getStatus().equals(GeneralStatus.ACTIVE)) {
             throw new AppBadException("status wrong !");
         }
-        //check
-        if(EmailUtil.isEmail(dto.getUsername())){
-            if(!emailHistoryService.check(dto.getUsername(), dto.getCode())){
+        // check
+        if (EmailUtil.isEmail(dto.getUsername())) {
+            if (!emailHistoryService.check(dto.getUsername(), dto.getCode())) {
                 throw new AppBadException("code wrong !");
             }
-        }
-        else if(SmsUtil.isPhone(dto.getUsername())) {
-            //todo
+        } else if (SmsUtil.isPhone(dto.getUsername())) {
+            // todo
             throw new AppBadException("Sms provider ishlamayapti.Email orqali  qiling !");
-//            if(!smsHistoryService.check(dto.getUsername(), dto.getCode())){
-//                throw new AppBadException("code wrong !");
-//            }
-        }else{
+            // if(!smsHistoryService.check(dto.getUsername(), dto.getCode())){
+            // throw new AppBadException("code wrong !");
+            // }
+        } else {
             throw new AppBadException("wrong username !");
         }
-        profileRepository.changePassword(profile.getId(),bc.encode(dto.getPassword()));
+        profileRepository.changePassword(profile.getId(), bc.encode(dto.getPassword()));
 
-        return new AppResponse<>("passwordi  "+dto.getUsername()+"ni  muaffaqiyatli o'zgartirildi !");
+        return new AppResponse<>("passwordi  " + dto.getUsername() + "ni  muaffaqiyatli o'zgartirildi !");
     }
 
-    public ProfileDTO getLogInResponse(ProfileEntity profile){
-        ProfileDTO response=new ProfileDTO();
+    public ProfileDTO getLogInResponse(ProfileEntity profile) {
+        ProfileDTO response = new ProfileDTO();
         response.setName(profile.getName());
         response.setUsername(profile.getUsername());
-        //setroles
+        // setroles
         response.setRoles(profileRoleRepository.getAllRolesListByProfileId(profile.getId()));
-        //setjwt
-        response.setJwt(JwtUtil.encode(profile.getUsername(),profile.getId(),response.getRoles()));
+        // setjwt
+        response.setJwt(JwtUtil.encode(profile.getUsername(), profile.getId(), response.getRoles()));
         return response;
     }
-
 
 }
